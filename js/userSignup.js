c840210c3f0b9de4f6b108ac9547a2dbab7cd274
UdokaSuccess <successudoka2004@gmail.com>
2026-05-30 13:00:47 +0100

    Created the login page, design and functionlaity. Corrected style errors in useSignup page layout.

--- js/userSignup.js
+++ js/userSignup.js
@@ -1,22 +1,21 @@
-const BASE_URL = 'https://emegent.esbatech.org';
+ const BASE_URL = 'https://emergent.esbatech.org';
 
-
- const firstname =  document.getElementById('firstname').value.trim(),
- const middlename = document.getElementById('surname').value.trim(),
- const  othername = document.getElementById('othernames').value.trim(),
- const  username = document.getElementById('username').value.trim(),
- const email = document.getElementById('email').value.trim(),
- const tel = document.getElementById('phone').value.trim(),
- const gender = document.getElementById('gender').value,
- const password= document.getElementById('password').value,
-
- const form =  document.getElementById('form'),
- const errorMsg = document.getElementById('errorMsg'),
+ const form =  document.getElementById('form');
+ const errorMsg = document.getElementById('errorMsg');
 
  
  const signUp = async (arg) => {
+  const firstname =  document.getElementById('firstname').value.trim();
+ const middlename = document.getElementById('surname').value.trim();
+ const  othername = document.getElementById('othernames').value.trim();
+ const  username = document.getElementById('username').value.trim();
+ const email = document.getElementById('email').value.trim();
+ const tel = document.getElementById('phone').value.trim();
+ const gender = document.getElementById('gender').value;
+ const password= document.getElementById('password').value;
+
     try {
-    const response = await fetch(`${BASE_URL}/signup`, {
+    const response = await fetch(`${BASE_URL}/signup.php`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ firstname, middlename, othername, username, email, tel, gender, password})
@@ -24,10 +23,11 @@ const BASE_URL = 'https://emegent.esbatech.org';
 
     const data = await response.json();
 
-    if (response.ok) {
-      window.location.href = '/login.html';
+    if (data.code === '200') {
+      window.location.href = '../pages/login.html';
     } else {
       errorMsg.textContent = data.message || 'Signup failed';
+      console.log(response)
     }
   }
     catch (err) {
@@ -39,5 +39,5 @@ const BASE_URL = 'https://emegent.esbatech.org';
 
   form.addEventListener('submit', (e) => {
   e.preventDefault();
-  signup();
+  signUp();
 });
